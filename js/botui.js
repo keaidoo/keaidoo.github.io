3831cdc45bbca00c69a1f3bd6702c379730c1f81
keaidoo <yaqian_wang@qqe.com>
2019-11-06 21:36:25 +0800

    Site updated: 2019-11-06 21:36:25

--- js/botui.js
+++ js/botui.js
@@ -2508,15 +2508,15 @@ function bot_ui_ini() {
     var botui = new BotUI("hello-mashiro")
     botui.message.add({
         delay: 800,
-        content: "Hi, there👋"
+        content: "Hi, welcome to here 👋"
     }).then(function() {
         botui.message.add({
             delay: 1100,
-            content: "这里是 Mashiro"
+            content: "这里是 可爱多的小站"
         }).then(function() {
             botui.message.add({
                 delay: 1100,
-                content: "一个可爱的孩子~"
+                content: "我相信点开这里的你，一定和我有某种缘分才能来到这个私人空间~"
             }).then(function() {
                 botui.action.button({
                     delay: 1600,
@@ -2555,7 +2555,7 @@ function bot_ui_ini() {
             }).then(function() {
                 botui.message.add({
                     delay: 1500,
-                    content: "向往技术却误入商科，但后来喜欢上了经济学…"
+                    content: "向往管理却误入计算机，但后来喜欢上了coding（hello world）…"
                 }).then(function() {
                     botui.message.add({
                         delay: 1200,
@@ -2563,20 +2563,20 @@ function bot_ui_ini() {
                     }).then(function() {
                         botui.message.add({
                             delay: 1500,
-                            content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
+                            content: "主攻 Python，熟悉HTML/CSS/JavaScript，略懂 Java（的单词拼写）"
                         }).then(function() {
                             botui.message.add({
                                 delay: 1500,
-                                content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
+                                content: "研究的方向，是数据挖掘（data mining）城市计算以及机器学习（machine learning）【的名词含义】"
                             }).then(function() {
                                 botui.message.add({
                                     delay: 1800,
-                                    content: "喜欢画画，希望有一天能够被称为画师"
+                                    content: "喜欢做手工，乐于尝试新鲜的东西。希望有一天能够体验有趣不同的小众职业"
                                 }).then(function() {
                                     botui.action.button({
                                         delay: 1100,
                                         action: [{
-                                            text: "为什么叫Mashiro呢？ 🤔",
+                                            text: "为什么叫可爱多呢？ 🤔",
                                             value: "why-mashiro"
                                         }]
                                     }).then(function(a) {
@@ -2592,49 +2592,49 @@ function bot_ui_ini() {
         thirdpart = function() {
             botui.message.add({
                 delay: 1E3,
-                content: "Mashiro以及站名都来自一部动画，因为和主角有一样的爱好~ 如果有兴趣可以找找首页上的视频~"
-            }).then(function() {
-                botui.action.button({
-                    delay: 1500,
-                    action: [{
-                        text: "为什么是白猫呢？ 🤔",
-                        value: "why-cat"
-                    }]
-                }).then(function(a) {
-                    fourthpart()
-                })
-            })
-        },
-        fourthpart = function() {
-            botui.message.add({
-                delay: 1E3,
-                content: "因为对GitHub有种执念… "
-            }).then(function() {
-                botui.message.add({
-                    delay: 1100,
-                    content: "而且我真的是猫控！"
-                }).then(function() {
-                    botui.action.button({
-                        delay: 1500,
-                        action: [{
-                            text: "域名有什么含意吗？(ง •_•)ง",
-                            value: "why-domain"
-                        }]
-                    }).then(function(a) {
-                        fifthpart()
-                    })
-                })
-            })
-        },
-        fifthpart = function() {
-            botui.message.add({
-                delay: 1E3,
-                content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
-            }).then(function() {
-                botui.message.add({
-                    delay: 1600,
-                    content: "那么，仔细看看我的博客吧？ ^_^"
-                })
+                content: "一个喜欢吃可爱多的孩子，喜欢一切可爱的事物~"
+                    //     }).then(function() {
+                    //         botui.action.button({
+                    //             delay: 1500,
+                    //             action: [{
+                    //                 text: "为什么是白猫呢？ 🤔",
+                    //                 value: "why-cat"
+                    //             }]
+                    //         }).then(function(a) {
+                    //             fourthpart()
+                    //         })
+                    //     })
+                    // },
+                    // fourthpart = function() {
+                    //     botui.message.add({
+                    //         delay: 1E3,
+                    //         content: "因为对GitHub有种执念… "
+                    //     }).then(function() {
+                    //         botui.message.add({
+                    //             delay: 1100,
+                    //             content: "而且我真的是猫控！"
+                    //         }).then(function() {
+                    //             botui.action.button({
+                    //                 delay: 1500,
+                    //                 action: [{
+                    //                     text: "域名有什么含意吗？(ง •_•)ง",
+                    //                     value: "why-domain"
+                    //                 }]
+                    //             }).then(function(a) {
+                    //                 fifthpart()
+                    //             })
+                    //         })
+                    //     })
+                    // },
+                    // fifthpart = function() {
+                    //     botui.message.add({
+                    //         delay: 1E3,
+                    //         content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
+                    //     }).then(function() {
+                    //         botui.message.add({
+                    //             delay: 1600,
+                    //             content: "那么，仔细看看我的博客吧？ ^_^"
+                    //         })
             })
         }
 }
